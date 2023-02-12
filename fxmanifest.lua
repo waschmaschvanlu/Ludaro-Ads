f44bba5a88db7a964a88c7d6e9c38ba2d32c20a7
ludaro <92580232+waschmaschvanlu@users.noreply.github.com>
2023-02-12 09:58:02 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'common'
+
+name 'Ludaro Ads'
+description 'This is a script to show ads do /ads to try it out'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
