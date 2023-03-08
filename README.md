d6595d61a1acba042f7b0a26b0e784e666827f3e
Ludaro <92580232+waschmaschvanlu@users.noreply.github.com>
2023-03-08 16:56:39 +0100

    completely reworked it
    the LADS system got a rework now working with ox notify instead :)

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ludaro-Ads
-this is an ESX Script that just does ads, /ads (CONFIGURE YOUR ADS IN THE CLIENT.LUA AND YOUR JOBS/GROUPS IN THE SERVER.LUA
--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'common'
-
-name 'Ludaro Ads'
-description 'This is a script to show ads do /ads to try it out'
-version '1.0.0'
-
-client_script 'client.lua'
-server_script 'server.lua'
